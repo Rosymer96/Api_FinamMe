a3c2200995cede47b55d50e1c397f34830250da7
Rosa Vela <rosymer96@gmail.com>
2025-07-18 12:19:58 +0200

    Models para controller de crear usuario

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+const e = require("express");
+const pool = require("../config/conexion");
+
+//Crear usuario
+
+const createUser = async (name, email, password) => {
+  const insert = "INSERT INTO user (name,email, password) VALUES (?,?,?)";
+
+  const [result] = await pool.query(insert, [name, email, password]);
+  return result;
+};
+
+const selectByEmail = async (email) => {
+  const select = "SELECT * FROM user WHERE email = ?";
+  const [result] = await pool.query(select, [email]);
+  return result[0];
+};
+
+
+
+module.exports = { createUser };
