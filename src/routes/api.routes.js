5228298195f75e9ca8b1307910dcc5a65c2e1623
Rosa Vela <rosymer96@gmail.com>
2025-09-22 12:52:39 +0200

    Ruta de getprofile anadida.

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -2,5 +2,4 @@ const router = require("express").Router();
 
 router.use("/expense", require("./api_routes/expense.routes"));
 router.use("/user", require("./api_routes/user.routes"));
-
 module.exports = router;
--- src/routes/api_routes/user.routes.js
+++ src/routes/api_routes/user.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-
+const auth = require("../../middelware/auth");
 const userControllers = require("../../controllers/user.controllers");
 
 router.post("/register", userControllers.registerUser);
 router.post("/login", userControllers.loginUser);
+router.get("/profile", auth.checkToken, userControllers.getUserProfile);
 
 module.exports = router;
     
